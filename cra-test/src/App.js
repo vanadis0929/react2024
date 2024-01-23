fb73fa8f1f151cfec1d0788402b84659566909f9
bo.kim <vanadis0929@gmail.com>
2024-01-23 15:36:16 +0900

    useEffect - cleanup 응용

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -8,16 +8,11 @@ function App() {
   };
 
   const Hello = () => {
-    const hihi = () => {
-      console.log('created :p ');
-      return byebye;
-    };
-    const byebye = () => {
-      console.log('destroyed :D ');
-    };
-
-    //useEffect 내부 에서 return 된 함수는 컴포넌트가 제거될때 실행된다
-    useEffect(hihi, []);
+    //useEffect 내부 에서 return 된 함수는 컴포넌트가 제거될때 실행된다, (컴포넌트가 제거되는 시점에 무언가 하고 싶은경우에 )
+    useEffect(() => {
+      console.log('hi :p ');
+      return () => console.log('bye :D ');
+    }, []);
     return <h1>hello !</h1>;
   };
 
